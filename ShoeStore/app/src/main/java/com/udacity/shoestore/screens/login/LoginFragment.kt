f15f632bc15a7f8794ac50e71df5dc41671a5876
Safynaz-Tarek <48590985+Safynaz-Tarek@users.noreply.github.com>
2022-08-13 22:57:48 +0200

    added some design to the login & welcome fragments

--- ShoeStore/app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
+++ ShoeStore/app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
@@ -19,6 +21,15 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentLoginBinding = DataBindingUtil.inflate(inflater,
         R.layout.fragment_login,container, false)
+
+        binding.loginButtonExisting.setOnClickListener { view: View ->
+            Navigation.findNavController(view).navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+        }
+
+        binding.signUpButton.setOnClickListener { view :View ->
+            Navigation.findNavController(view).navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+        }
+
 
         return binding.root
     }
